0247d31dd6f3071c296b8e0d99cd8fc49b5e129e
SpineSh4rk <spineshark@Pantheon.localdomain>
2026-01-30 21:02:59 +0100

    S6 clean up

--- src/data/tilesets/tilesets_set7.c
+++ src/data/tilesets/tilesets_set7.c
@@ -2,6 +2,3 @@
 #include "macros.h"
 
 static const u8 sBlob_5438f8_550708[] = INCBIN_U8("data/Blob_5438f8_550708.bin");
-
-// Temporarily moved here for alignment reasons
-//static const u8 sBlob_55071a_557bf0[] = INCBIN_U8("data/Blob_55071a_557bf0.bin");
