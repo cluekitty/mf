05e60030b95cbedc89ecc0ca1ca602fd55000cc4
SpineShark <249215007+SpineSh4rk@users.noreply.github.com>
2026-02-13 00:13:25 +0100

    New naming scheme

--- include/data/common_bgs.h
+++ include/data/common_bgs.h
@@ -3,15 +3,19 @@
 
 #include "types.h"
 
-extern const u32 sSectorElevatorRoom_Bg3[171];
-extern const u32 sSaXChaseRoomSpotlight_Bg0[168];
-extern const u32 sSaXChaseRoom_Bg3[210];
-extern const u32 sSectorFirstRoom_Bg3[277];
-extern const u32 sMeltdownRedFlash_Bg0[64];
-extern const u32 sGreenHexSpotlight_Bg0[168];
-extern const u32 sGreenHex_Bg3[217];
-extern const u32 sPurpleAndGreenPillars_Bg3[208];
-extern const u32 sFakeChozoStatueRoom_Bg3[355];
-extern const u32 sGreenPipes_Bg3[383];
+extern const u32 sBg3_SectorElevatorRoom[171];
+
+extern const u32 sBg0_SaXChaseRoomSpotlight[168];
+
+extern const u32 sBg3_SaXChaseRoom[210];
+extern const u32 sBg3_SectorFirstRoom[277];
+
+extern const u32 sBg0_MeltdownRedFlash[64];
+extern const u32 sBg0_GreenHexSpotlight[168];
+
+extern const u32 sBg3_GreenHex[217];
+extern const u32 sBg3_PurpleAndGreenPillars[208];
+extern const u32 sBg3_FakeChozoStatueRoom[355];
+extern const u32 sBg3_GreenPipes[383];
 
 #endif /* COMMON_BGS_H */
--- src/data/common_bgs.c
+++ src/data/common_bgs.c
@@ -2,52 +2,52 @@
 #include "macros.h"
 #include "gba/display.h"
 
-const u32 sSectorElevatorRoom_Bg3[171] = {
+const u32 sBg3_SectorElevatorRoom[171] = {
 	BGCNT_SIZE_256x512,
 	_INCBIN_U32("data/common/sector_elevator_room_bg3.tm.lz")
 };
 
-const u32 sSaXChaseRoomSpotlight_Bg0[168] = {
+const u32 sBg0_SaXChaseRoomSpotlight[168] = {
 	BGCNT_SIZE_512x256,
 	_INCBIN_U32("data/common/sax_chase_room_spotlight_bg0.tm.lz")
 };
 
-const u32 sSaXChaseRoom_Bg3[210] = {
+const u32 sBg3_SaXChaseRoom[210] = {
 	BGCNT_SIZE_512x256,
 	_INCBIN_U32("data/common/sax_chase_room_bg3.tm.lz")
 };
 
-const u32 sSectorFirstRoom_Bg3[277] = {
+const u32 sBg3_SectorFirstRoom[277] = {
 	BGCNT_SIZE_512x256,
 	_INCBIN_U32("data/common/sector_first_room_bg3.tm.lz")
 };
 
-const u32 sMeltdownRedFlash_Bg0[64] = {
+const u32 sBg0_MeltdownRedFlash[64] = {
 	BGCNT_SIZE_256x256,
 	_INCBIN_U32("data/common/meltdown_red_flash_bg0.tm.lz")
 };
 
-const u32 sGreenHexSpotlight_Bg0[168] = {
+const u32 sBg0_GreenHexSpotlight[168] = {
 	BGCNT_SIZE_512x256,
 	_INCBIN_U32("data/common/green_hex_spotlight_bg0.tm.lz")
 };
 
-const u32 sGreenHex_Bg3[217] = {
+const u32 sBg3_GreenHex[217] = {
 	BGCNT_SIZE_512x256,
 	_INCBIN_U32("data/common/green_hex_bg3.tm.lz")
 };
 
-const u32 sPurpleAndGreenPillars_Bg3[208] = {
+const u32 sBg3_PurpleAndGreenPillars[208] = {
 	BGCNT_SIZE_256x512,
 	_INCBIN_U32("data/common/purple_and_green_pillars_bg3.tm.lz")
 };
 
-const u32 sFakeChozoStatueRoom_Bg3[355] = {
+const u32 sBg3_FakeChozoStatueRoom[355] = {
 	BGCNT_SIZE_512x256,
 	_INCBIN_U32("data/common/fake_chozo_statue_room_bg3.tm.lz")
 };
 
-const u32 sGreenPipes_Bg3[383] = {
+const u32 sBg3_GreenPipes[383] = {
 	BGCNT_SIZE_512x256,
 	_INCBIN_U32("data/common/green_pipes_bg3.tm.lz")
 };
