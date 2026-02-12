7f10b5ce4b1b91e59014e0463c4e622d154b0955
SpineShark <249215007+SpineSh4rk@users.noreply.github.com>
2026-02-12 19:59:48 +0100

    Unblob common bgs

---
+++ include/data/common_bgs.h
@@ -0,0 +1,17 @@
+#ifndef COMMON_BGS_H
+#define COMMON_BGS_H
+
+#include "types.h"
+
+extern const u32 sSectorElevatorRoom_Bg3[171];
+extern const u32 sSaXChaseRoomSpotlight_Bg0[168];
+extern const u32 sSaXChaseRoom_Bg3[210];
+extern const u32 sSectorFirstRoom_Bg3[277];
+extern const u32 sMeltdownRedFlash_Bg0[64];
+extern const u32 sGreenHexSpotlight_Bg0[168];
+extern const u32 sGreenHex_Bg3[217];
+extern const u32 sPurpleAndGreenPillars_Bg3[208];
+extern const u32 sFakeChozoStatueRoom_Bg3[355];
+extern const u32 sGreenPipes_Bg3[383];
+
+#endif /* COMMON_BGS_H */
---
+++ src/data/common_bgs.c
@@ -0,0 +1,53 @@
+#include "types.h"
+#include "macros.h"
+#include "gba/display.h"
+
+const u32 sSectorElevatorRoom_Bg3[171] = {
+	BGCNT_SIZE_256x512,
+	_INCBIN_U32("data/common/sector_elevator_room_bg3.tm.lz")
+};
+
+const u32 sSaXChaseRoomSpotlight_Bg0[168] = {
+	BGCNT_SIZE_512x256,
+	_INCBIN_U32("data/common/sax_chase_room_spotlight_gb0.tm.lz")
+};
+
+const u32 sSaXChaseRoom_Bg3[210] = {
+	BGCNT_SIZE_512x256,
+	_INCBIN_U32("data/common/sax_chase_room_bg3.tm.lz")
+};
+
+const u32 sSectorFirstRoom_Bg3[277] = {
+	BGCNT_SIZE_512x256,
+	_INCBIN_U32("data/common/sector_first_room_gb3.tm.lz")
+};
+
+const u32 sMeltdownRedFlash_Bg0[64] = {
+	BGCNT_SIZE_256x256,
+	_INCBIN_U32("data/common/meltdown_red_flash_bg0.tm.lz")
+};
+
+const u32 sGreenHexSpotlight_Bg0[168] = {
+	BGCNT_SIZE_512x256,
+	_INCBIN_U32("data/common/green_hex_spotlight_bg0.tm.lz")
+};
+
+const u32 sGreenHex_Bg3[217] = {
+	BGCNT_SIZE_512x256,
+	_INCBIN_U32("data/common/green_hex_bg3.tm.lz")
+};
+
+const u32 sPurpleAndGreenPillars_Bg3[208] = {
+	BGCNT_SIZE_256x512,
+	_INCBIN_U32("data/common/purple_and_green_pillars_bg3.tm.lz")
+};
+
+const u32 sFakeChozoStatueRoom_Bg3[355] = {
+	BGCNT_SIZE_512x256,
+	_INCBIN_U32("data/common/fake_chozo_statue_room_bg3.tm.lz")
+};
+
+const u32 sGreenPipes_Bg3[383] = {
+	BGCNT_SIZE_512x256,
+	_INCBIN_U32("data/common/green_pipes_bg3.tm.lz")
+};
