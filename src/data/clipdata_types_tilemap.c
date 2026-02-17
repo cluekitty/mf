bc65c2606314b4351345f220160ca672f5000dff
SpineShark <249215007+SpineSh4rk@users.noreply.github.com>
2026-02-17 23:26:47 +0100

    default tileset initial

---
+++ src/data/clipdata_types_tilemap.c
@@ -0,0 +1,14 @@
+#include "types.h"
+#include "macros.h"
+
+const u8 sClipdataCollisionTypes[] = INCBIN_U8("data/Blob_3f0834_3f08e4.bin");
+
+const u8 sClipdataCollisionTypes_Test[] = INCBIN_U8("data/Blob_3f08e4_3f09e4");
+
+const u8 sTileset_0_Tilemap[1604] = INCBIN_U8("data/tilesets/0.tm");
+
+const u16 sTileset_0_Pal[14 * 16] = INCBIN_U16("data/tilesets/0.pal");
+
+const u16 sClipdataBehaviorTypes[] = INCBIN_U8("data/Blob_0x3f11e8_3f1348.bin");
+
+const u16 sClipdataBehaviorTypes_Test[] = INCBIN_U8("data/Blob_3f1348_3f1548.bin");
