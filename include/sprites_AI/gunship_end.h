30c872c1a32f1468159affd7af4a17da910d51b9
clue cat <gitlab@clue.cat>
2026-02-19 23:38:58 -0600

    set up file structure for gunshipend and related sprite ai

---
+++ include/sprites_AI/gunship_end.h
@@ -0,0 +1,27 @@
+#ifndef GUNSHIP_END_H
+#define GUNSHIP_END_H
+
+#include "types.h"
+
+void GunshipEndSpawnBeams(void);
+void GunshipEndInit(void);
+void GunshipEndWaiting(void);
+void GunshipEndMovingUp(void);
+void GunshipEndWaitingToEnter(void);
+void GunshipEndEntering(void);
+void GunshipEndWaitingForSamus(void);
+void GunshipEndLockingSamus(void);
+void GunshipEndMovingSamusUp(void);
+void GunshipEndStartingEngine1(void);
+void GunshipEndStartingEngine2(void);
+void GunshipEndTakingOff(void);
+void GunshipEndDone(void);
+void GunshipEndBottomIdle(void);
+void GunshipEndBeamInit(void);
+void GunshipEndBeamMovingDown(void);
+void GunshipEndBeamMovingUp(void);
+void GunshipEnd(void);
+void GunshipEndBottom(void);
+void GunshipEndBeam(void);
+
+#endif /* GUNSHIP_END_H */
---
+++ src/sprites_AI/gunship_end.c
@@ -0,0 +1,124 @@
+#include "globals.h"
+#include "macros.h"
+
+#include "constants/sprite.h"
+
+#include "structs/sprite.h"
+
+void GunshipEndSpawnBeams(void)
+{
+
+}
+
+
+void GunshipEndInit(void)
+{
+
+}
+
+
+void GunshipEndWaiting(void)
+{
+
+}
+
+
+void GunshipEndMovingUp(void)
+{
+
+}
+
+
+void GunshipEndWaitingToEnter(void)
+{
+
+}
+
+
+void GunshipEndEntering(void)
+{
+
+}
+
+
+void GunshipEndWaitingForSamus(void)
+{
+
+}
+
+
+void GunshipEndLockingSamus(void)
+{
+
+}
+
+
+void GunshipEndMovingSamusUp(void)
+{
+
+}
+
+
+void GunshipEndStartingEngine1(void)
+{
+
+}
+
+
+void GunshipEndStartingEngine2(void)
+{
+
+}
+
+
+void GunshipEndTakingOff(void)
+{
+
+}
+
+
+void GunshipEndDone(void)
+{
+
+}
+
+
+void GunshipEndBottomIdle(void)
+{
+
+}
+
+
+void GunshipEndBeamInit(void)
+{
+
+}
+
+
+void GunshipEndBeamMovingDown(void)
+{
+
+}
+
+
+void GunshipEndBeamMovingUp(void)
+{
+
+}
+
+
+void GunshipEnd(void)
+{
+
+}
+
+
+void GunshipEndBottom(void)
+{
+
+}
+
+
+void GunshipEndBeam(void) {
+
+}
